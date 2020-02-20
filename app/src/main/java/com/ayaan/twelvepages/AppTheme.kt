1b2bb65f2187b4498c4a626fa11b84c6e596dbf3
kyeonghun.seo@day2life.com <kyeonghun.seo@day2life.com>
2020-02-20 15:57:13 +0900

    Merge branch 'master' of https://github.com/KyeonghunSeo/Chating
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	.idea/misc.xml

--- app/src/main/java/com/ayaan/twelvepages/AppTheme.kt
+++ app/src/main/java/com/ayaan/twelvepages/AppTheme.kt
@@ -56,8 +56,8 @@ object AppTheme {
         dimColor = resource.getColor(R.color.dim)
         hightlightCover = resource.getDrawable(R.drawable.highlightcover)
         blankDrawable = resource.getDrawable(R.drawable.blank)
-        thinFont = ResourcesCompat.getFont(context, R.font.write_right)!!
-        regularFont = ResourcesCompat.getFont(context, R.font.write_right)!!
-        boldFont = ResourcesCompat.getFont(context, R.font.write_right)!!
+        thinFont = ResourcesCompat.getFont(context, R.font.thin_s)!!
+        regularFont = ResourcesCompat.getFont(context, R.font.regular_s)!!
+        boldFont = ResourcesCompat.getFont(context, R.font.bold_s)!!
     }
 }
